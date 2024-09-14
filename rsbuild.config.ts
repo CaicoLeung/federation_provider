a3b115928617af27198da1166e37271e3b6ca804
Caico Leung <lkk1053190902@gmail.com>
2024-09-14 13:44:51 +0800

    init: federation_provider

---
+++ rsbuild.config.ts
@@ -0,0 +1,30 @@
+import { defineConfig } from '@rsbuild/core';
+import { pluginReact } from '@rsbuild/plugin-react';
+import { ModuleFederationPlugin } from '@module-federation/enhanced/rspack';
+
+export default defineConfig({
+  plugins: [pluginReact()],
+  server: {
+    port: 3000
+  },
+  dev: {
+    // It is necessary to configure assetPrefix, and in the production environment, you need to configure output.assetPrefix
+    assetPrefix: true
+  },
+  tools: {
+    rspack: {
+      output: {
+        uniqueName: 'federation_provider'
+      },
+      plugins: [
+        new ModuleFederationPlugin({
+          name: 'federation_provider',
+          exposes: {
+            './button': './src/Button.tsx'
+          },
+          shared: ['react', 'react-dom'],
+        })
+      ]
+    }
+  }
+});
