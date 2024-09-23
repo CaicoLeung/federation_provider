2957388b6fccee6373aae8692f0576d2ae9e80d0
Caico Leung <lkk1053190902@gmail.com>
2024-09-23 23:17:51 +0800

    feat ✨: Add msw for mocking API responses and configure dependencies

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,18 @@
+import { useCallback, useEffect } from 'react';
 import './App.css';
 
 const App = () => {
+  const request = useCallback(async () => {
+    const res = await fetch('https://example.com/user')
+    console.log('res', res);
+  }, [])
+
+  useEffect(() => {
+    (async () => {
+      await request()
+    })()
+  }, [request])
+
   return (
     <div className="content">
       <h1>I am MF Sub App</h1>
--- src/bootstrap.tsx
+++ src/bootstrap.tsx
@@ -2,10 +2,14 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
+import { worker } from './mock/mock';
 
 const root = ReactDOM.createRoot(document.getElementById('root') as HTMLElement);
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-);
+
+worker.start().then(() => {
+  root.render(
+    <React.StrictMode>
+      <App />
+    </React.StrictMode>,
+  );
+})
